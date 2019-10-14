0334db91033407a5509e476007bfa19ba076ba07
haihan yin <haihan.yin@rabobank.nl>
2019-10-14 13:41:26 +0200

    add arraylist and linkedlist

---
+++ src/test/java/com/github/haihan/lc/java/util/ArrayListTest.java
@@ -0,0 +1,17 @@
+package com.github.haihan.lc.java.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    @Test
+    public void test() {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+
+}
---
+++ src/test/java/com/github/haihan/lc/java/util/LinkedListTest.java
@@ -0,0 +1,16 @@
+package com.github.haihan.lc.java.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    public void test() {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+}
